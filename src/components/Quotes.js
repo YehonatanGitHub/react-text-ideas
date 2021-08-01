3da7587df2f63effde1cac72e00ca6992e945550
Jonathan R <2140173@gmail.com>
2021-08-02 00:03:01 +0300

    add mobile and desktop detect tags

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,38 +1,44 @@
 import whatsapp from './whatsapp.png'
+import { BrowserView, MobileView } from 'react-device-detect'
 
 const Quotes = ({ quotes, i }) => {
   return (
     <div className='quote' key={i}>
       <div className='quote-content'>"{quotes.content}"</div>
       <div className='quote-author'>- {quotes.author} -</div>
-      <a
-        href={
-          'whatsapp://send?text=' +
-          quotes.content +
-          ' - ' +
-          quotes.author +
-          ' - sent from www.ShareGreatQuotes.com'
-        }
-        className='ns_btn'
-        data-action='share/whatsapp/share'
-        target='_blank'
-        rel='noreferrer'>
-        <img src={whatsapp} width='35' height='35' alt='whatsapp-icon' />
-      </a>
-      <a
-        href={
-          'https://web.whatsapp.com/send?text=' +
-          quotes.content +
-          ' - ' +
-          quotes.author +
-          ' - sent from www.ShareGreatQuotes.com'
-        }
-        className='ns_btn'
-        data-action='share/whatsapp/share'
-        target='_blank'
-        rel='noreferrer'>
-        <img src={whatsapp} width='35' height='35' alt='whatsapp-icon' />
-      </a>
+      <MobileView>
+        <a
+          href={
+            'whatsapp://send?text=' +
+            quotes.content +
+            ' - ' +
+            quotes.author +
+            ' - built by https://yrwebdev.com'
+          }
+          className='ns_btn'
+          data-action='share/whatsapp/share'
+          target='_blank'
+          rel='noreferrer'>
+          <img src={whatsapp} width='35' height='35' alt='whatsapp-icon' />
+        </a>
+      </MobileView>
+      <BrowserView>
+        <a
+          href={
+            'https://web.whatsapp.com/send?text=' +
+            quotes.content +
+            ' - ' +
+            quotes.author +
+            ' - built by https://yrwebdev.com'
+          }
+          className='ns_btn'
+          data-action='share/whatsapp/share'
+          target='_blank'
+          rel='noreferrer'>
+          <img src={whatsapp} width='35' height='35' alt='whatsapp-icon' />
+        </a>
+      </BrowserView>
+
       <hr />
     </div>
   )
